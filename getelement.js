3720784d6f3f79c6df16f810a297a24057c87a2b
laxatzi <laxatzi@yahoo.com>
2023-01-12 12:23:24 +0200

    INIT-getelement.html with recipe page content and getelemetns.js as empty file

---
+++ getelement.js
@@ -0,0 +1,6 @@
+// How to get elements in the DOM
+// querySelector()
+// use querySelector() to find a matching element on the page. Any valid css selector can be used.
+
+// this method follows the same pattern as css and enables developers to use complex selectors
+// for example const fishsoup_second_ingredient = document.querySelector(".fish_recipes .fishsoup li:nth-child(7)")
