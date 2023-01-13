a64525fbbe4faaab9263abc071496651cb38f79a
laxatzi <laxatzi@yahoo.com>
2023-01-13 20:37:22 +0200

    ADD-example of querySelectorAll() method with pseudo classes

--- DOM/HowToGetElements/getelement.js
+++ DOM/HowToGetElements/getelement.js
@@ -41,17 +41,22 @@
 // const el = document.querySelector("div.user-panel:not(.main) input[name='login']");
 
 // useful links
-// https://gomakethings.com/what-to-do-when-queryselector-fail-on-valid-selectors/
-// https://gomakethings.com/the-javascript-selector-methods-that-changed-everything/
 // https://gomakethings.com/challenges-and-limitations-with-advanced-selectors-and-the-document.queryselectorall-method/
 // https://gomakethings.com/live-vs.-static-nodelists-and-htmlcollections-in-vanilla-js/
 // https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
 
 // With querySelector() and querySelectorAll() you can get the first element in the DOM that have one of two or more selectors
 // To make it work, pass in your desired selectors separated with a comma, all within quotes as a single argument.
-
-// In my example i could select spices and vegetables from the ingredient list
 const fish_recipes =  document.querySelectorAll('[data-fish-recipe], .fish');
 	for(let fish_recipe of fish_recipes) {
     fish_recipe.style.background = "beige";
   }
+
+  // You can even use pseudo-classes. Let’s say you want to ignore buttons with the .tuna class. You can use the :not() pseudo-class in your selector string, like this…
+
+// let notTuna = document.querySelectorAll('.sandwich:not(.tuna)');
+const only_meat_recipes = document.querySelectorAll('.recipes:not([data-recipe="fish"])');
+
+for(let meat_recipe of only_meat_recipes) {
+  meat_recipe.style.background = "pink";
+}
