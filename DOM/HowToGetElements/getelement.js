3bb8a0f50a7ddf7733c873b7e16a26df1d0ab561
laxatzi <laxatzi@yahoo.com>
2023-01-16 09:36:48 +0200

    ADD files

--- DOM/HowToGetElements/getelement.js
+++ DOM/HowToGetElements/getelement.js
@@ -1,4 +1,9 @@
 // How to get elements in the DOM
+// By far, the most versatile method, elem.querySelectorAll(css) returns all elements inside elem matching the given CSS selector.
+// The call to elem.querySelector(css) returns the first element for the given CSS selector.
+
+// In other words, the result is the same as elem.querySelectorAll(css)[0], but the latter is looking for all elements and picking one, while elem.querySelector just looks for one. So it’s faster and also shorter to write.
+
 // querySelector()
     // use querySelector() to find a matching element on the page. Any valid css selector can be used.
 
@@ -15,7 +20,7 @@
   // static means that it doesn’t change, even if the UI does.
   // suppose we want to add a new ingredient in the fishsoup recipe
   const fishsoup_ingredients = document.querySelector(".fishsoup ul");
-  let ingredients = fishsoup_ingredients.querySelectorAll('li');
+
 
   //  for (let ingredient of ingredients) {
 
@@ -29,7 +34,7 @@
 // 	li.textContent = 'Chilly pepper';
 // 	fishsoup_ingredients.append(li);
 
-// 	// logs the first three buttons, but not the new one
+// 	// logs  not the new one
 // 	for(let ingredient of ingredients) {
 //     console.log(ingredient.textContent);
 //   }
@@ -37,13 +42,9 @@
 // }, 3000);
 
 
-//You can also negate selectors:
+
 // const el = document.querySelector("div.user-panel:not(.main) input[name='login']");
 
-// useful links
-// https://gomakethings.com/challenges-and-limitations-with-advanced-selectors-and-the-document.queryselectorall-method/
-// https://gomakethings.com/live-vs.-static-nodelists-and-htmlcollections-in-vanilla-js/
-// https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
 
 // With querySelector() and querySelectorAll() you can get the first element in the DOM that have one of two or more selectors
 // To make it work, pass in your desired selectors separated with a comma, all within quotes as a single argument.
@@ -52,11 +53,14 @@ const fish_recipes =  document.querySelectorAll('[data-fish-recipe], .fish');
     fish_recipe.style.background = "beige";
   }
 
-  // You can even use pseudo-classes. Let’s say you want to ignore buttons with the .tuna class. You can use the :not() pseudo-class in your selector string, like this…
-
-// let notTuna = document.querySelectorAll('.sandwich:not(.tuna)');
+  // You can even use pseudo-classes, like negate selectors:Let’s say you want to ignore buttons with the data-recipe="fish" attribute. You can use the :not() pseudo-class in your selector string, like this…
 const only_meat_recipes = document.querySelectorAll('.recipes:not([data-recipe="fish"])');
 
 for(let meat_recipe of only_meat_recipes) {
   meat_recipe.style.background = "pink";
 }
+// and now the meat recipe section is selected
+// notes from MDN
+//https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+//https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+
