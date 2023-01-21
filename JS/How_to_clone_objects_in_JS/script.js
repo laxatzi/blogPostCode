53cde4e844e95122712ad382e691d979aca1bd00
laxatzi <laxatzi@yahoo.com>
2023-01-21 20:41:43 +0200

    ADD-example of a deep copy of an object with comments

--- JS/How_to_clone_objects_in_JS/script.js
+++ JS/How_to_clone_objects_in_JS/script.js
@@ -30,3 +30,49 @@ const car = {
     console.log(employee_clone);
 }
 // As we can see, the change in age in the original obj doesn't reflect to the copy obj.
+
+// Both the Object.assign() method  and the spread syntax operator are sufficient for creating shallow copies of an object.
+// but not deep copies.
+// With multi-dimensional object, any change in a nested object would also affect the original object.
+
+// example
+{
+    const car = {
+        type: "SUV",
+        manufacturer: "Renault",
+        model: "Captur",
+        fuel: {
+             standard: "oil",
+             hybrid: "lng/oil",
+           }
+    };
+    const copy_car = Object.assign({}, car);
+    copy_car.fuel.standard = 'gasoline';
+    console.log(car);
+    console.log(copy_car);
+
+}
+
+// original array is affected. The same would have happened if we used the spread syntax operator d
+
+
+// How to create a deep copy of an obj with JS
+// The best way to create a deep copy is by using the structuredClone() native method..
+// In this method we pass the  object we want to clone in as an argument.
+// Then it returns a value that is a deep copy of the original value.
+// In order to achieve this it uses the structured clone algorithm [link]
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm
+
+
+const marine_biology = {
+  fish: ["sea-bass", "sardine"],
+  jellyfish: ["Cotylorhiza tuberculata", "Pelagia noctiluca"],
+};
+
+const med_marine_biology = structuredClone(marine_biology);
+//After cloning, changes to each object do not affect the other object.
+med_marine_biology.jellyfish.push("Moon jelly");
+marine_biology.jellyfish.pop();
+
+console.log(med_marine_biology); //
+console.log(marine_biology); //
