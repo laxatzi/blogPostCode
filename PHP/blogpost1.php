3936e1a1473de26098f06fb98a13d4a6ff09ea90
laxatzi <laxatzi@yahoo.com>
2023-12-15 17:09:59 +0200

    Expand more on scopes

--- PHP/blogpost1.php
+++ PHP/blogpost1.php
@@ -1,5 +1,10 @@
 <?php
- // A Comprehensive Guide to PHP Functions
+ // PHP Functions from a JS programmer perspective
+
+ // This is meant to be a comprehensive post about PHP functions. Since JS was my first language, I tend to compare everything I learn, outside of the JavaScript world, with the syntax and peculiarities of that beloved by me, hated by a lot of people, language.
+
+ // If PHP is the first language you wish to get hands-on experience with, hold the line.
+ // The post is also catered to the absolute beginner. Just ignore the remarks about JS.
 
 //  A function is a block of code designed to carry out a certain task.
 //  It can be used repeatedly within our codebase, enabling cleaner, easier to maintain, and more legible code.
@@ -22,7 +27,7 @@
 say_hi(); // Hi
 
 // Return a value
-// In the above examples we just displayed the result of the functions for demonstration purposes.
+// In the above example we just displayed the result of the functions for demonstration purposes.
 // In reality, functions are returning code rather than displaying it.
 // a return statement forces the function to cease execution.
 
@@ -55,18 +60,20 @@ function greet_name3($greeting, $name) {
 
 echo greet_name3("Hello", "Slay"); // null is returned resulting in displaying an empty page
 
+// Function Parameters
 // Inside the parens, we can make use of parameters.
 
 function name($arg_1, $arg_2, /* ..., */ $arg_n) {
   // Statements...
 }
+
 // Function parameters are not mandatory, and there is no limit to how many you can use.
 // Parameters are a convenient way to create variables that are scoped to the function, thus are locally defined. The are also convenient regarding naming, since we can give them any name that aligns with the purpose of our program.
 
 // Note that although arguments, and parameters are concepts that are used interchangeably, they are not the same.
 // Parameters are variables passed to a function or method in order to be used by that function when it is called.
 // Arguments are the values of that variables.
-// So in our example $first_name is the variable and "John" is the argument.
+// So in the bellow example $first_name is the parameter and "John" is the argument.
 
  function fn_name($first_name, $last_name) {
    // code to be executed
@@ -84,7 +91,7 @@ greet_name("Hello", "John"); // Hello John
 echo "<br>";
 greet_name("Good morning", "Maria"); // Good morning Maria
 
-// We see that we can call the function multiple times with different arguments and different results.
+// We see that we can call the function multiple times with different arguments having different results.
 // By using parameters, we can take a variable from a function scope, and give its value to a local scope.
 // More specifically to our example, breaking down the different scopes:
   // -Function Scope:
@@ -96,7 +103,7 @@ greet_name("Good morning", "Maria"); // Good morning Maria
     // second call arguments $greeting="Good morning" , $name = "Maria"
     // We notice that regarding both calls the parameter is maintaining the integrity of the argument outside that function scope.
     // Put it simply, when we assign a value to one call, the other is not affected. We can greet both "John" and "Maria" using the same parameter ($name).
-    // For those of you who doesn't come from another language and/or haven't met with the concept of scope yet I'll try to explain it above.
+    // For those of you who doesn't come from another language and/or haven't met with the concept of scope yet I'll try to explain it bellow.
 
 // A few words about Scopes
 
@@ -128,7 +135,29 @@ function what_pet() {
 
 echo what_pet(); // My pet is a Cat
 
-// Another way , the preferred way since it adheres to the concept of encapsulation, is to pass the global variable as a parameter.
+// Now if we echo out the $pet variable outside of the local scope my pet is still also a Cat
+echo 'My pet is a '.$pet;
+
+// Changing the global variable inside the local scope, in the above example, want affect the global variable's value prior to the function being called.
+
+function what_pet3() {
+  // local scope
+  global $pet;
+  // reassign it to Dog
+  $pet = 'Dog';
+  return 'My pet is a '. $pet;
+}
+
+echo 'My pet is a '.$pet; // My pet is still a Cat
+
+echo "<br>";
+echo what_pet3(); // My pet is a Dog inside the function
+
+// But if we echo it out after the function is called the pet becoming a Dog
+echo "<br>";
+echo 'My pet is a '. $pet;
+
+// Another way, the preferred way since it adheres to the concept of encapsulation, is to pass the global variable as a parameter.
 // Note that encapsulation is the  the practice of bundling related data into a structured unit [link wikipedia]
 // So now everything we need 'lives' in that function and everything from the outside is coming in as an argument.
 
@@ -139,6 +168,8 @@ function what_pet2($pet) {
 echo what_pet2($pet); // My pet is a Cat
 
 
+
+
 // Default arguments
   // In PHP, you can set a default argument for a parameter.
   // This is handy for cases where a specific value is very common
@@ -147,6 +178,7 @@ echo what_pet2($pet); // My pet is a Cat
   function hyphenated_word($word1, $word2, $joiner='-'){
     return $word1.$joiner.$word2;
   }
+
 // When you call the hyphenated_word() function and don’t pass the $joiner argument, the function will use the '-' as the default argument:
 echo  hyphenated_word('short', 'term'); // short-term
 
@@ -163,6 +195,7 @@ function hyphenated_word2($joiner='-', $word1, $word2) {
 }
 
 echo hyphenated_word('short', 'term'); // Uncaught ArgumentCountError
+
 // This is happening because arguments are parsed to function left-to-right,
 // In our example, we get the error because the first argument (short) we have provided to hyphenated_word2() function gets assigned to $joiner,overriding the '-' value. This is making $word2 unassigned.
 
@@ -174,7 +207,7 @@ echo hyphenated_word('short', 'term'); // Uncaught ArgumentCountError
   // How to use Variadic parameters in PHP
   // In Programming, a variadic function is a function of indefinite arity, i.e., one which accepts a variable number of arguments
   // Variadic functions are handy in cases where we do not know all the parameters our function needs at the time of definition.
-  // If you come from a js background like I do, it is the equivelant of the spread operator.
+  // If you come from a js background, it is the equivalent of the spread operator.
   // Lets see an example:
   // We create a function and we want to be able to take in as many numbers as we want and add them together.
   // So we use the splat operator, also known as three dots operator.
@@ -194,9 +227,7 @@ echo hyphenated_word('short', 'term'); // Uncaught ArgumentCountError
   echo "<br>";
   echo add_numbers(2, 3, 4, 5, 25); // 39
 
-  // Function Scope
-  // Scope is an important concept in programming.
-  //
+
 
 
 
