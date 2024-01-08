2a92432eecda046e4ab1e04f025664ee5e8d8105
laxatzi <laxatzi@yahoo.com>
2024-01-08 15:53:37 +0200

    Update post intro

--- PHP/dataTypes.php
+++ PHP/dataTypes.php
@@ -1,4 +1,11 @@
 <?php
 
+// Exploring Data Types in PHP
 
+ // In programming we instruct computers to do things.
+ // We call this instructions programs.
+ // We can write programs using diff programming languages, the same way we can express our feelings using different human languages.
+ // Each language consists of data, and each data has a type.
+ // The operations that the computer performs are based on these types.
+ // Various data types require different memory allocation and support different operations.
 
