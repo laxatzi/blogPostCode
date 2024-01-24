c733be5b1f23a6d9356b53a3b34f56c398ddf8ba
laxatzi <laxatzi@yahoo.com>
2024-01-24 16:40:09 +0200

    Write about the GLOBALS superglobal

--- PHP/php_superglobals.php
+++ PHP/php_superglobals.php
@@ -2,14 +2,15 @@
 
 // Superglobals are special built-in variables in PHP, that are available from any function, class, or file without previous declaration of them as global variables.
 
-// What that practically means is that there is no need to use the  global keyword to access them.
+// That practically means, there is no need to use the global keyword to access them.
 
-// Bellow is how we can access a global variable from a function:
+// Bellow is how we can access a common global variable from a function:
 
 $x = 10;
 $y = 2;
 
 function subtract() {
+  // Without the global keyword we'd get an 'Undefined variable' notice, and the result would be different (0)
   global $x, $y;
   return $x - $y;
 }
@@ -19,15 +20,101 @@ echo subtract(); // 8
 // Superglobals do not need the global keyword
 {
   session_start();
-  /*session is started if you don't write this line can't use $_Session  global variable*/
-
-  $_SESSION["newsession"]= 1.1;
+  $_SESSION["newSession"]= 1.1;
 
   function get_session() {
-
   // no need for the global keyword
-  return $_SESSION["newsession"];
+  return $_SESSION["newSession"];
   }
 
   echo get_session(); // 1.1
 }
+
+// We're going to be talking about how the $_SESSION superglobal works later on, but for now just pay attention to how we get access to the global scope without the use of the 'global' keyword.
+
+// There are nine superglobals in PHP.
+// These superglobal variables are:
+
+    // $_GLOBALS: It holds a reference to all the global variables that have been defined in a PHP script.
+    // $_GET: contains the parameters passed to the script through the query string.
+    // $_POST: contains data sent via an HTTP POST request.
+    // $_COOKIE: contains data passed to the script as cooky values.
+    // $_SESSION: contains data stored in a user's session.
+    // $_REQUEST: contains all the data sent by user's client.
+    // $_SERVER: contains information about the server.
+    // $_ENV: contains information about the environment.
+
+    // Becoming familiar with the basics of each superglobal makes data manipulation in web applications easy and simple.
+
+    // The $_GLOBALS Variable in PHP
+    // The $_GLOBALS variable is an associative array that references all variables in global scope, with the variable names being the keys of the array.
+
+    // Let’s see how the $_GLOBALS variable can be used to access a global variable with an example:
+
+$age = 10;
+$name = "Tommy";
+
+function get_students_age() {
+
+  return $GLOBALS['name']. " is " .$GLOBALS['age'] ." years old!";
+}
+
+echo get_students_age(); //Tommy is 10 years old!
+
+// Apart from accessing global variables, $GLOBALS can be used to modifying them.
+// In the same example:
+{
+  $age = 10;
+  $name = "Tommy";
+
+function get_students_age2() {
+
+  return $GLOBALS['name']. " is " .$GLOBALS['age']=12 ." years old!";
+}
+
+ echo get_students_age2(); //Tommy is 12 years old!
+}
+
+// We can possibly use this superglobal in order to pass variables between functions
+// Examples:
+{
+  function first_number(){
+    $GLOBALS['a'] = 1;
+    echo "First number: " . $GLOBALS['a']."<br>";
+    ++$GLOBALS['a'];
+
+    // NOTE: This is a closure. We'll talk extensively about closures in a later post
+    $second_number = function(){
+        echo "Second number: ". $GLOBALS['a'];
+    };
+
+    $second_number();
+  };
+
+ first_number(); // First number: 1, Second number: 2
+}
+
+// But there is a better, less verbose, way to do the same thing with the USE keyword.
+// Used in an anonymous function the ‘use’ keyword enables us to introduce, in that scope, variables from an outer scope.
+
+{
+    function the_first_number(){
+      $a = 1;
+      echo "First number: " . $a."<br>";
+      ++$a;
+
+      // NOTE: This is a closure. We'll talk extensively about closures in a later post
+      $the_second_number = function() USE ($a){
+          echo "Second number: ". $a;
+      };
+
+    $the_second_number();
+  };
+
+  the_first_number(); //First number: 1, Second number: 2
+}
+
+
+
+
+
