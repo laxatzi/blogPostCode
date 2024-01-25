1bf27f986a46c0542e65822f520ec168da6c4e41
laxatzi <laxatzi@yahoo.com>
2024-01-25 19:45:58 +0200

    Write about server superglobal variables

--- PHP/php_superglobals.php
+++ PHP/php_superglobals.php
@@ -47,7 +47,7 @@ echo subtract(); // 8
 
     // Becoming familiar with the basics of each superglobal makes data manipulation in web applications easy and simple.
 
-    // The $_GLOBALS Variable in PHP
+    // # The $_GLOBALS Variable in PHP
     // The $_GLOBALS variable is an associative array that references all variables in global scope, with the variable names being the keys of the array.
 
     // Let’s see how the $_GLOBALS variable can be used to access a global variable with an example:
@@ -96,7 +96,7 @@ function get_students_age2() {
 }
 
 // But there is a better, less verbose, way to do the same thing with the USE keyword.
-// Used in an anonymous function the ‘use’ keyword enables us to introduce, in that scope, variables from an outer scope.
+// Used in an anonymous function the ‘use’ keyword enables us to introduce, in that function scope, variables from outer scope.
 
 {
     function the_first_number(){
@@ -119,16 +119,16 @@ function get_students_age2() {
 // So for the post PHP 8.1 versions of PHP, global variables cannot be modified through their copy.
 
 //Example:
- // Before PHP 8.1
+ // Before PHP 8.1:
   $email = 'myemail@gmail.com';
   $globals_copy = $GLOBALS;
   $globals_copy['email'] = 'mysecondemail@gmail.com';
   echo $email; // mysecondemail@gmail.com
   echo $globals_copy['email']; // mysecondemail@gmail.com
   echo $GLOBALS['email'];  // mysecondemail@gmail.com
- // By modifying the copy we do modify the $GLOBALS array itself
+ // By modifying the copy we do modify the $GLOBALS array as well.
 
- // After PHP 8.1
+ // From version PHP 8.1 and on:
  {
   $email = 'myemail@gmail.com';
   $globals_copy = $GLOBALS;
@@ -154,7 +154,7 @@ function get_students_age2() {
  }
  // We can list all available elements with the following code:
  {
-  foreach ($_SERVER as $parm => $value)  echo "$parm = '$value' <br>";
+  foreach ($_SERVER as $param => $value)  echo "$param = '$value' <br>";
  }
  // It's going to print, depending in our server config, something similar to the above image:
  // [site the image]
@@ -162,12 +162,84 @@ function get_students_age2() {
 // We are going to look at some of the most commonly used elements of the PHP $_SERVER superglobal.
 // A complete list is available in the PHP documentation [link: https://www.php.net/manual/en/reserved.variables.server.php]
 
-// $_SERVER['REQUEST_METHOD']: It returns the request method that is used to access the page.
+// $_SERVER['REQUEST_METHOD']:
+// It returns the request method that is used to access the page.
 // This can be a 'POST', a 'GET', a 'PUT' method e.t.c
 // Example:
 {
-  echo $_SERVER["REQUEST_METHOD"]; // GET
+  echo "The server request method is: ". $_SERVER["REQUEST_METHOD"]; // GET
 }
+
+// It stores the name and version of the protocol of the request like HTTP 1, or HTTP/2 e.t.c
+// Example:
+{
+  echo "The server protocol is: " . $_SERVER['SERVER_PROTOCOL']; // HTTP/1.0
+}
+
+// // $_SERVER['SERVER_NAME']:
+// It is used in order to retrieve  the name of the host server.
+// Example:
+{
+  echo "The server name is: " . $_SERVER['SERVER_NAME']; // The server name is: mywebsite.local
+}
+
+// // $_SERVER['SERVER_PORT']:
+//  It returns the port number the server machine (not the client’s machine) use for communication.
+// Example:
+{
+  echo "The server port is: " . $_SERVER['SERVER_PORT']; // The server port is: 10023
+}
+
+// // $_SERVER['SERVER_SOFTWARE']:
+// It is used to get the server identification string, that contains details of the server software.  
+// Example:
+{
+  echo "The server software is: " . $_SERVER['SERVER_SOFTWARE']; // The server name is:  Apache/2.4.43 (Win32) mod_fcgid/2.3.9a
+}
+
+
+// // $_SERVER['SERVER_ADMIN']:
+// It is used to store the email address of the server administrator/webmaster.
+// Example:
+{
+  echo "The server admin is: " . $_SERVER['SERVER_ADMIN']; // The server admin is: webmaster@localhost
+}
+
+
+// // $_SERVER['DOCUMENT_ROOT']:
+// It returns the root of the document. That's where the script that we are running resides.
+// Example:
+{
+  echo "The document root is: " . $_SERVER['SERVER_ADMIN']; // The document root is: C:/Users/LAMBROS/Local Sites/mywebsite/app/public
+}
+
+
+// // $_SERVER['SCRIPT_FILENAME']:
+// It returns not just the filename as the name implies, but the absolute pathname of the currently executed PHP script.
+// Example:
+{
+  echo "The script filename is: " . $_SERVER['SCRIPT_FILENAME']; //  C:/Users/LAMBROS/Local Sites/mywebsite/app/public/test.php
+}
+
+
+// // $_SERVER['SCRIPT_NAME']:
+// It returns the script name of the currently executed PHP file. It doesn't include the path-info nor a URL query string. It always includes a leading slash.
+// Example:
+{
+  echo "The script name is: " . $_SERVER['SCRIPT_NAME']; // /test.php
+}
+
+// // $_SERVER['PHP_SELF']:
+// Just like $SERVER['SCRIPT_NAME'], it returns the file name of the script currently executed on the server. It does include the URL query string though.
+// Example:
+{
+ echo "The file name of the current script currently executed on the server is: " . $_SERVER['PHP_SELF']; // The file name of the script currently executed on the server is: /test.php
+}
+
+
+
+
+
 
 
 
