9e4c468bb89e447d7a466650932703e252f4378c
laxatzi <laxatzi@yahoo.com>
2024-01-30 11:32:54 +0200

    Update envs.bat

---
+++ PHP/envs.bat
@@ -0,0 +1,6 @@
+PATH=>C:\WINDOWS\System32;C:\Program Files\Common Files\Oracle\Java\javapath;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\Git\cmd;C:\Program Files\Go\bin;C:\Program Files\Amazon\AWSCLIV2\;C:\Hugo\bin;C:\Program Files\nodejs\;C:\Users\LAMBROS&STELLA\AppData\Local\Programs\Python\Python310\Scripts\;C:\Users\LAMBROS&STELLA\AppData\Local\Programs\Python\Python310\;C:\Users\LAMBROS&STELLA\AppData\Local\Microsoft\WindowsApps;C:\Users\LAMBROS&STELLA\go\bin;C:\Users\LAMBROS&STELLA\AppData\Local\Programs\Microsoft VS Code\bin;C:\Users\LAMBROS&STELLA\AppData\Roaming\npm
+SYSTEMROOT=>C:\WINDOWS
+WINDIR=>C:\WINDOWS
+PHP_FCGI_MAX_REQUESTS=>1000
+PHPRC=>C:/Users/LAMBROS&STELLA/AppData/Roaming/Local/run/KHHGXYQgd/conf/php
+_FCGI_SHUTDOWN_EVENT_=>1432
