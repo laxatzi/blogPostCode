b1f0a98eacd5c2b4b8497038e60c1033562ba05a
laxatzi <laxatzi@yahoo.com>
2024-02-15 15:06:42 +0200

    init post about js types

---
+++ JS/javascriptTypes.js
@@ -0,0 +1,7 @@
+//# Converting Types in JavaScript
+
+// JavaScript is a dynamically typed, also known as untyped, language.
+
+// In dynamically typed languages, typing is associated with the variable’s value and not the variable itself.
+
+// Thus, in JavaScript, variables don’t need to specify a certain data type when they are declared.
