901d8dacabbde838a2aa4312035aba48249e3614
laxatzi <laxatzi@yahoo.com>
2024-02-29 16:08:52 +0200

    Conclud post about conditional statements

--- PHP/conditional_statements_in_php.php
+++ PHP/conditional_statements_in_php.php
@@ -3,23 +3,23 @@
 
    /*
 
-    When we want to submit a form by clicking the send button, or show a warning message next to empty required fields, we use conditional statements.
+  When we want to submit a form by clicking the send button, or show a warning message next to empty required fields, we use conditional statements.
 
-    With the use of conditional statements, we are able to evaluate conditions and determine which code should be executed.
+  With the use of conditional statements, we are able to evaluate conditions and determine which code should be executed.
 
-    We create conditional statements using the if, else, elseif, and switch keywords together with comparison operators.
+  We create conditional statements using the if, else, elseif, and switch keywords together with comparison operators.
 
-    So let's see them one by one:
+  So let's see them one by one:
 
    */
 
   ##The If statement
 
-  // The syntax should be like:
+  // The syntax:
   if ($condition) {
   // code to be executed
 }
-// If the condition in the parens evaluates to true, the code will be executed.
+// If the condition in the parentheses evaluates to true, the code will be executed.
 // For example:
 
 
@@ -33,13 +33,13 @@ if ($age >= 18) {
 
 // NOTE: The curly braces can be omitted entirely if there is only one statement to execute;
 
-// In that case the previous example would be written as:
+// In that case, the previous example would be written as:
   if ($age  >= 18 ) echo "Welcome to our Bar";
 
 
   ##The If-Else statement
 
-  // But what if the customer, in our example, is not of the right age and we want to display a message regarding  the reason for our denial to grant him access to our bar?
+  // But what if the customer, in our example, is not of the right age and we want to display a message regarding the reason for our denial to grant him access to our bar?
   // We can use the If-Else statement:
   //If the condition evaluates to true, the code within the if block will be executed, while the code within the else block will only be executed when the condition is false.
   if ($age >= 18 ) {
@@ -47,13 +47,13 @@ if ($age >= 18) {
   } else {
     echo "Sorry but we cannot grant you access to our Bar! You are only ". $age . " old!";
   }
-  // Now if the customer is too young to be granted access to our Bar, we can at least explain the reason with a message!
+  // Now if the customer is too young to be granted access to our bar, we can at least explain the reason with a message!
 
-   // There are cases when executing either one piece of code or another would not suffice.
+   // There are cases when executing either one piece of code or the other would not suffice.
    // In these cases we need to use the elseif keyword.
 
    // Let's say we want to respond to a question about age eligibility for driving a car.
-   // In certain countries you can drive a car at 17 if accompanied by a licensed driver at least 25 years old.
+   // In certain countries, you can drive a car at 17 if accompanied by a licensed driver at least 25 years old.
   $age = 19;
   if ($age >= 18 ) {
     echo "You can certainly drive a car if you have a license";
@@ -81,9 +81,9 @@ if ($age >= 18) {
    }
 
    // Output: You can certainly drive a car if you have a licenseYou can drive a car if you have a license and you are accompanied by a licensed driver at least 25 years old
-   // Now we get two messages in a row. this is because PHP checks both if statements. Even if the first statement has been evaluated to true it want stop.
+   // Now we get two messages in a row. This is because PHP checks both if statements. Even if the first statement has been evaluated to true it won't stop.
 
-   // Note If the conditions are not in the correct order, this could result in an inaccurate result.
+   // Note: If the conditions are not in the correct order, this could result in an inaccurate result.
   $age = 19;
   if ($age >= 17 ) {
     echo "You can drive a car if you have a license and you are accompanied by a licensed driver at least 25 years old";
@@ -95,12 +95,13 @@ if ($age >= 18) {
    // Output: You can drive a car if you have a license and you are accompanied by a licensed driver at least 25 years old
    // Here we have reversed the order of the conditions to check for. The 17 years benchmark is checked first, but it leads to
    // an inaccurate result.
-  ##Nested If statements
 
+
+  ##Nested If statements
 
   //Conditional statements can be nested inside other conditional statements in cases where there are many conditions. In such nested If statements, we can greatly enhance readability with consistent indentation.
 
-  // Let's say we want to add another condition in our previous example. With proper indentation and proper nesting we can avoid a the mess of too many conditions:
+  // Let's say we want to add another condition in our previous example. With proper indentation and proper nesting we can avoid the mess of too many conditions:
 
       if ($age >= 17) {
          if ($age > 18) {
@@ -116,7 +117,7 @@ if ($age >= 18) {
          }
     }
 
-// NOTE: PHP accepts spaces between else and if thus the above code could be written as:
+// NOTE: PHP accepts spaces between else and if thus, the above code could be written as:
       $age = 19;
 
 
@@ -134,8 +135,8 @@ if ($age >= 18) {
          }
     }
 
-// Nesting is not the only option when dealing with a lot of conditions:
-// Regarding the prev example, we could remove nesting with the use of elseif statements as some small changes in the conditional statements.
+// Nesting is not the only option when dealing with a lot of conditions.
+// Regarding the previous example, we could remove nesting with the use of elseif statements as some small changes in the conditional statements.
 
 
 if ($age < 16) {
@@ -151,8 +152,8 @@ elseif ($age > 18) {
     echo "You can certainly drive drive a car if you have a license!";
 }
 
-// Our code now is a little cleaner, but not neccesarrily more readable. Readability has a lot to do with the context.
-// Our code should tell a story that is easily comprehensible by other programmers. Here if our main goal is to answer the question "At what age can a teenager drive a car?", then the former example is more readable. If we want to answer the question "What are the rights of a teenager regarding driving respective to his/her age?" then the later is the one more readable.
+// Our code now is a little cleaner, but not necessarily more readable. Readability has a lot to do with the context.
+// Our code should tell a story that is easily comprehensible by other programmers. Here, if our main goal is to answer the question "At what age can a teenager drive a car?", then the former example is more readable. If we want to answer the question "What are the rights of a teenager regarding driving respective to his/her age?" then the latter is the one more readable.
 
 ## Alternate syntax for conditional statements
 // PHP provides an alternative syntax for if/else conditional statements.
@@ -165,8 +166,8 @@ elseif ($age > 18) {
   elseif ($age >= 17) :
            echo "You can drive a car if you have a license and you are accompanied by a licensed driver at least 25 years old";
   else :
-    echo "You cannot drive a car yet!";
-   endif;
+           echo "You cannot drive a car yet!";
+  endif;
 
 // NOTE:
 // Using a space between if and else is not acceptable in the alternate syntax.
@@ -179,9 +180,10 @@ elseif ($age > 18) {
   else :
     echo "You cannot drive a car yet!";
    endif;
+// OUtcome: Parse error: syntax error, unexpected token "if", expecting ":" in Standard input code on line 6
 
 ##Embedding if statements in HTML
-  // When embedding if/else conditions inside HTML the alternate syntax is recommended for better readability.
+  // When embedding if/else conditions inside HTML, the alternate syntax is recommended for better readability.
   ?>
 <div class="message-display">
 
@@ -212,12 +214,12 @@ elseif ($age > 18) {
 <?php
 
   ## The Null Coalescing Operator
-  // As of PHP7 a new logical operator has been introduced that is very similar to the ternary operator.
+  // As of PHP 7,  a new logical operator has been introduced that is very similar to the ternary operator.
   // It is the null coalescing operator (??) that returns its first operand if it is not null or undefined; otherwise it returns its second operand.
   // Syntax:
   $first_operand ?? $second_operand;
 
-  // The Null Coalescing Operator is in esense syntactic sugar for cases where we need to use a ternary operator along with the isset() method.
+  // The Null Coalescing Operator is in reality syntactic sugar for cases where we need to use a ternary operator along with the isset() method.
 // The following example uses a ternary operator:
 $fav_color = "red";
 
@@ -233,9 +235,7 @@ $color = $fav_color ?? "green";
 
   ## The Switch Statement
 
-  //[ The switch statement is similar to an if statement, except that it does not work with a range of values. A switch statement requires each case to be based on a single value. Depending on the value of the variable used for switching, the program will execute the correct block of code. ]
-
-    // The switch conditional statement, evaluates an expression based on different cases, with each case based on a single value. The program executes the respective statements, depending on each value, when the expression is matched and until the break statement is reached.
+    // The switch conditional statement, evaluates an expression against different cases, with each case returning a single value. The program executes the statements, returning the respective value, when the expression is matched and until the break statement is reached.
     // There can be a default, optional, case denoted with the default statement, which is used when all previous cases remain unmatched.
 
     // Syntax:
@@ -250,9 +250,9 @@ $color = $fav_color ?? "green";
     // Example:
 
 echo "Enter your grade: ";
-$userGrade = "A";
+$user_grade = "A";
 
-switch ($userGrade) {
+switch ($user_grade) {
     case "A+":
     case "A":
         echo "Distinction\n";
@@ -267,11 +267,16 @@ switch ($userGrade) {
     default:
         echo "Fail\n";
 }
+// Output: "Distinction
+//          Well Done!"
+/*
+  If the grade is “A+” (Line 15), the program executes the next statement until it reaches the break statement. This means it’ll execute lines 16 to 19.
 
-/* [
-    After getting the user’s grade, we use the switch statement that follows to determine the output. If the grade entered is “A+” (Line 15), the program executes the next statement until it reaches the break statement. This means it’ll execute lines 16 to 19. Thus, the output is “Distinction”, followed by “Well Done!”. If grade is “A” (Line 16), the program executes lines 17 to 19. Similarly, the output is “Distinction”, followed by “Well Done!”. If grade is not “A+” or “A”, the program checks the next case. It keeps checking from top to bottom until a case is satisfied. If none of the cases apply, the default case is executed.]
+If the grade is “A” (Line 16), the program executes lines 17 to 19.  
 
+If a grade is neither “A+” nor “A”, the program iterates through the cases, checking from top to bottom, until it finds a case that is satisfied.
 
+If none of the cases is valid, the default case will be executed.
 */
 ?>
 
